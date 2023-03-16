b282ec6f0d3228d42be7546f98887ce7a7cf1d37
engadnanhossain <engr.adnanhossain@gmail.com>
2023-03-16 23:40:51 +0600

    Added local storage

--- again-product/product.js
+++ again-product/product.js
@@ -8,5 +8,39 @@ const getInputField = id => {
 const addStudentInfo = () => {
    const nameValue =  getInputField('name-field');
    const ageValue = getInputField('age-field');
-   localStorage.setItem(nameValue, ageValue);
+//    localStorage.setItem(nameValue, ageValue);
+   addInfoList(nameValue, ageValue);
+   savedStudentInfo(nameValue, ageValue);
+};
+
+const getStudentInfo = () => {
+    const info = localStorage.getItem('student');
+    let student = {};
+    if(info){
+        student = JSON.parse(info);
+    }
+    return student;
+};
+
+const savedStudentInfo = (nameValue, ageValue) => {
+    const student = getStudentInfo();
+    student[nameValue] = ageValue;
+    const infoStringfy = JSON.stringify(student);
+    localStorage.setItem('student', infoStringfy);
 }
+
+const addInfoList = (nameValue, ageValue) => {
+    const infoList = document.getElementById('infoList');
+    const li = document.createElement('li');
+    li.innerText = `${nameValue} ${ageValue}`;
+    infoList.appendChild(li);
+};
+
+const displayStudentValue = () => {
+   const student =  getStudentInfo()
+   for(const nameValue in student){
+    const ageValue = student[nameValue];
+    addInfoList(nameValue, ageValue)
+   }
+}
+displayStudentValue()
