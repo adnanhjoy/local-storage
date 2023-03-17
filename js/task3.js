fe9618d87e7fd97be74cc5defdd407d934cf5a47
engadnanhossain <engr.adnanhossain@gmail.com>
2023-03-17 18:19:13 +0600

    Added localstorage

--- js/task3.js
+++ js/task3.js
@@ -1,14 +1,60 @@
 const getInputField = id => {
 const inputField = document.getElementById(id);
 const inputValue = inputField.value;
+inputField.value = '';
 return inputValue;
 };
 
 const sendData = () => {
     const nameField = getInputField('name-field');
-    localStorage.setItem('name', nameField)
+    localStorage.setItem('name', nameField) 
+    return nameField;
+};
+
+const emailSendData = () => {
+    const emailField = getInputField('email-field');
+    localStorage.setItem('email', emailField)
+    return emailField;
+}
+const messageSendData = () => {
+    const messageField = getInputField('message-field');
+    localStorage.setItem('message', messageField)
+    return messageField;
 }
 
 const deletData = () => {
     localStorage.removeItem('name')
 }
+const emailDeletData = () => {
+    localStorage.removeItem('email')
+}
+const messageDeletData = () => {
+    localStorage.removeItem('message')
+}
+
+const resetButton = () => {
+    localStorage.clear()
+};
+
+const getPersonData = () => {
+    const info = localStorage.getItem('userInfo');
+    let userInfo = {};
+    if(info){
+        userInfo = JSON.parse(info);
+    }
+    return userInfo
+};
+
+const savedPersonData = (nameValue, emailValue, messageValue) => {
+    const userInfo  = getPersonData();
+    userInfo[nameValue] = emailValue;
+    const emaliStringify = JSON.stringify(userInfo);
+    localStorage.setItem('userInfo', emaliStringify);
+}
+
+const saveButton = () => {
+    const nameValue = sendData()
+    const emailValue = emailSendData()
+    const messageValue = messageSendData()
+    console.log(nameValue, emailValue, messageValue)
+};
