67686c51524cfc78a714512271c5eaf1b903bd07
engadnanhossain <engr.adnanhossain@gmail.com>
2023-03-17 15:19:52 +0600

    Added localstorage

--- js/task1.js
+++ js/task1.js
@@ -7,17 +7,27 @@ const getInputValue = id => {
 const getStudentInfo = () => {
 const nameValue = getInputValue('name-field');
 const rollValue = getInputValue('roll-field');
-localStorage.setItem(nameValue, rollValue)
+// localStorage.setItem(nameValue, rollValue);
+addStudentList(nameValue, rollValue);
+savedStudentInfo(nameValue, rollValue);
+
 };
 
 const getStudentList = () => {
-    const info = localStorage.getItem('info');
+    const info = localStorage.getItem('student');
     let student = {};
     if(info){
         student = JSON.parse(info);
     }
     return student;
-}
+};
+
+const savedStudentInfo = (nameValue, rollValue) => {
+    const student = getStudentList();
+    student[nameValue] = rollValue;
+    const studentStringify = JSON.stringify(student);
+    localStorage.setItem('student', studentStringify);
+};
 
 const addStudentList = (nameValue,rollValue) => {
     const studentList = document.getElementById('student-list');
@@ -25,3 +35,12 @@ const addStudentList = (nameValue,rollValue) => {
     li.innerText = `${nameValue} ${rollValue}`;
     studentList.appendChild(li);
 };
+
+const displayStudentValue = () => {
+    const student = getStudentList()
+    for(const nameValue in student){
+        const rollValue = student[nameValue]
+        addStudentList(nameValue, rollValue)
+    }
+}
+displayStudentValue();
